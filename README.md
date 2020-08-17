184a5d7274b4d9f352f30437965e68771ee86c3f
loskutov-surf <loskutov@surfstudio.ru>
2020-08-17 18:12:11 +0300

    Add check env at script startup, some fixes, add README

---
+++ README.md
@@ -0,0 +1,14 @@
+# k8s-shell-scripts
+Shell scripts for setup Kubernetes
+
+
+### Install Kubernetes with CRI-O runtime on CentOS 8
+
+**Prerequisites:**
+* I recommend at least one host with minimum 2 cores, 4 GB RAM, 15 GB HDD  
+* Installed CentOS 8 (I recommend "Server" profile)
+
+**Install:**  
+Setup version of CRI-O and Kubernetes and run with `sudo` (or from `root`):
+
+    sudo bash -c "export REQUIRED_VERSION=1.18 && ./crio-centos8/k8s-crio-setup.sh"
--- crio-centos8/k8s-crio-setup.sh
+++ crio-centos8/k8s-crio-setup.sh
@@ -1,10 +1,8 @@
 #!/bin/env bash
 
-# Set Kubernetes & CRI-O version
-
-if [[ -z "$REQUIRED_VERSION" ]]; then
+if [[ ! -z "$REQUIRED_VERSION" ]]; then
     
-    echo "Kubernetes & CRI-O version is $REQUIRED_VERSION\n"
+    echo -e ">>> Kubernetes & CRI-O version is $REQUIRED_VERSION\n"
 
     # Update all packets
     dnf -y update
@@ -25,10 +23,10 @@ if [[ -z "$REQUIRED_VERSION" ]]; then
 
     # Enable forwarding
     cat > /etc/sysctl.d/99-kubernetes-cri.conf <<EOF
-    net.bridge.bridge-nf-call-iptables = 1
-    net.ipv4.ip_forward = 1
-    net.bridge.bridge-nf-call-ip6tables = 1
-    EOF
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
     sysctl --system
 
     # Setup CRI-O repos
@@ -44,15 +42,15 @@ if [[ -z "$REQUIRED_VERSION" ]]; then
 
     # Setup K8s repo
     cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-    [kubernetes]
-    name=Kubernetes
-    baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
-    enabled=1
-    gpgcheck=1
-    repo_gpgcheck=1
-    gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-    exclude=kubelet kubeadm kubectl
-    EOF
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
 
     # Install K8s
     dnf install -y kubelet-$REQUIRED_VERSION* kubeadm-$REQUIRED_VERSION* kubectl-$REQUIRED_VERSION* --disableexcludes=kubernetes
@@ -60,18 +58,21 @@ if [[ -z "$REQUIRED_VERSION" ]]; then
     # Configure K8s
     mkdir /var/lib/kubelet
     cat <<EOF > /var/lib/kubelet/config.yaml
-    apiVersion: kubelet.config.k8s.io/v1beta1
-    kind: KubeletConfiguration
-    cgroupDriver: systemd
-    EOF
-    cat /dev/null > access.log
+apiVersion: kubelet.config.k8s.io/v1beta1
+kind: KubeletConfiguration
+cgroupDriver: systemd
+EOF
+    
+    cat /dev/null > /etc/sysconfig/kubelet
     cat <<EOF > /etc/sysconfig/kubelet
-    KUBELET_EXTRA_ARGS=--container-runtime=remote --cgroup-driver=systemd --container-runtime-endpoint='unix:///var/run/crio/crio.sock'
-    EOF
+KUBELET_EXTRA_ARGS=--container-runtime=remote --cgroup-driver=systemd --container-runtime-endpoint='unix:///var/run/crio/crio.sock'
+EOF
     sudo systemctl enable --now kubelet
+
+    echo -e "\n>>> Now you can initiate K8s with \`kubeadm init\` command"
 
 else
 
-    echo "Please setup Kubernetes & CRI-O version as \$REQUIRED_VERSION"
+    echo ">>> Please setup Kubernetes & CRI-O version as \$REQUIRED_VERSION"
 
 fi
